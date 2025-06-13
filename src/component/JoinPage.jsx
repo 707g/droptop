2eb72e53ecd520ef62c455be390272c48430ab0e
707g <daandaan707@gmail.com>
2025-06-13 17:10:20 +0900

    login&join header, title

--- src/component/JoinPage.jsx
+++ src/component/JoinPage.jsx
@@ -1,10 +1,91 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IoMdHome } from "react-icons/io";
 
-function JoinPage(props) {
+const JoinHeader = styled.div`
+  width: 100%;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 952px;
+    height: 200px;
+  }
+`;
+
+const JoinTitle = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+
+  .event_in {
+    width: 952px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+
+    p {
+      color: #6B6B6B;
+    }
+
+    .event_title {
+      width: 952px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      h1 {
+        font-weight: 600;
+        color: #0A2986;
+      }
+
+      .nav {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        color: #999;
+
+        svg {
+          margin-right: 4px;
+          font-size: 18px;
+          vertical-align: middle;
+        }
+
+        .current {
+          color: #0A2986;
+          font-weight: bold;
+        }
+
+        span {
+          margin: 0 4px;
+        }
+      }
+    }
+  }
+`;
+
+function JoinPage() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <JoinHeader>
+        <img src={process.env.PUBLIC_URL + "/images/join/mainImg.png"} alt="mainImg" />
+      </JoinHeader>
+      <JoinTitle>
+        <div className='event_in'>
+          <div className='event_title'>
+            <h1>회원가입</h1>
+            <div className="nav">
+              <IoMdHome />
+              HOME <span>&gt;</span>
+              <span className="current">회원가입</span>
+            </div>
+          </div>
+          <p>간단한 정보입력을 통한 회원가입을 진행합니다.</p>
+        </div>
+      </JoinTitle>
+    </>
   );
 }
 
--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,10 +1,91 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IoMdHome } from "react-icons/io";
 
-function LoginPage(props) {
+const LoginHeader = styled.div`
+  width: 100%;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 952px;
+    height: 200px;
+  }
+`;
+
+const LoginTitle = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+
+  .event_in {
+    width: 952px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+
+    p {
+      color: #6B6B6B;
+    }
+
+    .event_title {
+      width: 952px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      h1 {
+        font-weight: 600;
+        color: #0A2986;
+      }
+
+      .nav {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        color: #999;
+
+        svg {
+          margin-right: 4px;
+          font-size: 18px;
+          vertical-align: middle;
+        }
+
+        .current {
+          color: #0A2986;
+          font-weight: bold;
+        }
+
+        span {
+          margin: 0 4px;
+        }
+      }
+    }
+  }
+`;
+
+function LoginPage() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <LoginHeader>
+        <img src={process.env.PUBLIC_URL + "/images/login/mainImg.png"} alt="mainImg" />
+      </LoginHeader>
+      <LoginTitle>
+        <div className='event_in'>
+          <div className='event_title'>
+            <h1>로그인</h1>
+            <div className="nav">
+              <IoMdHome />
+              HOME <span>&gt;</span>
+              <span className="current">로그인</span>
+            </div>
+          </div>
+          <p>회원님의 아이디와 비밀번호를 확인 후 로그인을 진행해주세요.</p>
+        </div>
+      </LoginTitle>
+    </>
   );
 }
 
